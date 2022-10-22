3699e81d205f17f8d66484b16f0c6bed4c4c4e60
Clyne Fuentes <clippefuentes@gmail.com>
2022-10-22 16:16:50 -0600

    add game logic

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,9 @@ struct Obstacle {
 struct State {
     player: Player,
     frame_time: f32,
+    obstacle: Obstacle,
     mode: GameMode,
+    score: i32,
 }
 
 impl GameState for State {
@@ -43,8 +45,10 @@ impl State {
         State {
             player: Player::new(5, 25),
             frame_time: 0.0,
+            obstacle: Obstacle::new(SCREEN_WIDTH, 0),
             mode: GameMode::Menu,
-        }
+            score: 0,
+          }
     }
 
     fn play(&mut self, ctx: &mut BTerm) {
@@ -60,15 +64,28 @@ impl State {
         }
         self.player.render(ctx);
         ctx.print(0, 0, "Press SPACE to flap.");
-        if self.player.y > SCREEN_HEIGHT {
-            self.mode = GameMode::End;
+        ctx.print(0, 1, &format!("Score: {}", self.score)); 
+
+        self.obstacle.render(ctx, self.player.x); 
+        if self.player.x > self.obstacle.x { 
+          self.score += 1;
+          self.obstacle = Obstacle::new(
+              self.player.x + SCREEN_WIDTH, self.score
+          );
+        }
+        if self.player.y > SCREEN_HEIGHT || 
+            self.obstacle.hit_obstacle(&self.player)
+        {
+          self.mode = GameMode::End;
         }
     }
 
     fn restart(&mut self) {
         self.player = Player::new(5, 25);
         self.frame_time = 0.0;
+        self.obstacle = Obstacle::new(SCREEN_WIDTH, 0);
         self.mode = GameMode::Playing;
+        self.score = 0;
     }
 
     fn main_menu(&mut self, ctx: &mut BTerm) {
@@ -89,6 +106,7 @@ impl State {
     fn dead(&mut self, ctx: &mut BTerm) {
         ctx.cls();
         ctx.print_centered(5, "You are dead!");
+        ctx.print_centered(6, &format!("You earned {} points", self.score));
         ctx.print_centered(8, "(P) Play Again");
         ctx.print_centered(9, "(Q) Quit Game");
 
